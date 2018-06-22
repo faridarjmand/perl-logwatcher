4c7c505f4774a557f8a7d4bc9888bbdd1656b284
Farid Arjmand <faridarjmand73@gmail.com>
2018-06-23 00:49:51 +0430

    Update logwatcher.pl

--- logwatcher.pl
+++ logwatcher.pl
@@ -1,13 +1,17 @@
 #!/bin/perl
-##########################################
+################# Created By.Farid Arjmand ################
+
 use strict;
 #use warnings;
 use IO::File;
 use File::Tail;
 #use Term::ANSIColor;
 use Switch;
-##########################################
-# Color modifiers
+
+##########################################################
+################### Color modifiers ######################
+##########################################################
+
 # print(colored([$mod, $fg, $bg], ' Text '));
 #my @mods = ('', 'bold', 'dark', 'underline', 'blink', 'reverse', 'concealed');
 #my @fgs  = ('black', 'red', 'blue', 'yellow', 'green', 'magenta', 'cyan', 'white');
@@ -20,35 +24,31 @@ my $GREEN="\e[1;1;42m"; 	# Background Green
 my $RED="\e[1;1;41m"; 		# Background Red
 my $YELLO="\e[1;1;43m"; 	# Background Yellow
 my $nc="\e[0m";			# No Color
-##########################################
+
+##########################################################
+########################## MAIN ##########################
+##########################################################
+
 main(@ARGV);
-##########################################
+
+##########################################################
+####################### Functions ########################
+##########################################################
+
 sub main
 {
 	my $Switch = $ARGV[0];
+	my $FILENAME = $ARGV[1];
+	print("Your Switch is : [ $yelloo$Switch$nc ] \n\n");
+	error("missing filename") unless $FILENAME;
 	switch ($Switch)
 	{
-		case '-tail'
-		{
-			my $FILENAME = $ARGV[1];
-			print("Your Switch is : [ $yelloo$Switch$nc ] \n\n");
-			error("missing filename") unless $FILENAME;
-			tail ($FILENAME);
-		}
-		case '-count'
-		{
-			my $FILENAME = $ARGV[1];
-			print("Your Switch is : [ $yelloo$Switch$nc ] \n\n");
-			error("missing filename") unless $FILENAME;
-			countines ($FILENAME);
-		}
-		else
-		{
-			print "\nUsage: tail.pl switch:[-tail|-count] file_name\n";
-		}
+		case '-tail' {tail ($FILENAME);}
+		case '-count' {count ($FILENAME);}
+		else {print "\nUsage: tail.pl switch:[-tail|-count] file_name\n";}
 	}
 }
-sub countines
+sub count
 {
 	my $FILENAME = shift;
 	my $COUNTFILE = IO::File->new( $FILENAME, "r"  ) or error("can't open $FILENAME ($!)\n");
@@ -73,3 +73,7 @@ sub error
 	print("$0: $e\n");
 	exit 0;
 }
+
+##########################################################
+########################## END ###########################
+##########################################################
